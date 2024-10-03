a3d38631533e751318971481adf62c2b84ce156e
Squashim <konrad18v1@wp.pl>
2024-10-03 17:30:17 +0200

    Added form validation

--- frontend/src/Pages/RegisterPage/Register.tsx
+++ frontend/src/Pages/RegisterPage/Register.tsx
@@ -1,74 +1,108 @@
 import { Link } from "react-router-dom";
 import styles from "../LoginPage/Login.module.scss";
 
-import useInput from "../../hooks/useInput";
-import Input from "../../components/Input/Input";
-import { FormEvent } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+
+const schema = z
+	.object({
+		companyName: z
+			.string()
+			.min(3, "Nazwa musi mieć co najmniej 3 znaki")
+			.max(25, "Nazwa nie może mieć więcej niż 25 znaków")
+			.regex(/^[A-Za-z\s.]+$/, "Nazwa może mieć tylko litery i spacje"),
+		email: z.string().email("Niepoprawny adres email"),
+		password: z
+			.string()
+			.min(8, "Hasło musi mieć co najmniej 8 znaków")
+			.regex(/[A-Za-z]/, {
+				message: "Hasło musi zawierać przynajmniej jedną literę",
+			})
+			.regex(/\d/, { message: "Hasło musi zawierać przynajmniej jedną cyfrę" })
+			.regex(/^[A-Za-z\d]+$/, {
+				message: "Hasło może zawierać tylko litery i cyfry",
+			}),
+		confirmPassword: z.string().min(8, "Potwierdzenie hasła jest wymagane"),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		path: ["confirmPassword"],
+		message: "Hasła muszą być takie same",
+	});
+
+type FormFields = z.infer<typeof schema>;
 
 const Register = () => {
-	const companyNameInput = useInput("");
-	const emailInput = useInput("");
-	const passwordInput = useInput("");
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors, isSubmitting },
+	} = useForm<FormFields>({
+		resolver: zodResolver(schema),
+	});
 
-	const handleSubmit = (e: FormEvent) => {
-		e.preventDefault();
-		validateInput(companyNameInput);
-		validateInput(emailInput);
-		validateInput(passwordInput);
-	};
+	const onSubmit: SubmitHandler<FormFields> = async (data) => {
+		try {
+			const { companyName, email, password } = data;
 
-	const validateInput = (input: {
-		value: string;
-		setError: (value: boolean) => void;
-	}) => {
-		if (!input.value.trim()) {
-			input.setError(true);
-		} else {
-			input.setError(false);
+			const response = await axios.post("http://localhost:8080/auth/signup", {
+				email: email,
+				password: password,
+				companyName: companyName,
+			});
+
+			console.log(response.data.message);
+		} catch (error) {
+			console.log(error);
+			// setError("email", {
+			// 	message: "Istnieje użytkownik o podanym adresie email!",
+			// });
 		}
 	};
 
 	return (
 		<main className={styles.main}>
 			<h1>Rejestracja</h1>
-			<form onSubmit={handleSubmit}>
-				<Input
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<label>Nazwa firmy:</label>
+				<input
+					{...register("companyName")}
 					type='text'
-					label='Nazwa'
-					name='name'
-					placeholder='Wpisz nazwę'
-					{...companyNameInput}
+					name='companyName'
+					placeholder='Firma'
 				/>
-				<Input
+				{errors.companyName && <p>{errors.companyName.message}</p>}
+				<label>email:</label>
+				<input
+					{...register("email")}
 					type='email'
-					label='Email'
 					name='email'
-					placeholder='Wpisz email'
-					{...emailInput}
+					placeholder='Email'
 				/>
-				<Input
+				{errors.email && <p>{errors.email.message}</p>}
+				<label>hasło:</label>
+				<input
+					{...register("password")}
 					type='password'
-					label='Hasło'
 					name='password'
-					placeholder='Wpisz hasło'
-					{...passwordInput}
+					placeholder='Hasło'
 				/>
+				{errors.password && <p>{errors.password.message}</p>}
+				<label>powtórz hasło:</label>
+				<input
+					{...register("confirmPassword")}
+					type='password'
+					name='confirmPassword'
+					placeholder='Potwierdź hasło'
+				/>
+				{errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
 
-				<button type='submit'>Wyślij</button>
-
-				{/* <label htmlFor='company_name'>Nazwa firmy</label>
-				<input type='text' name='company_name' id='company_name' />
-
-				<label htmlFor='email'>Adres email</label>
-				<input type='email' name='email' id='email' />
-
-				<label htmlFor='passw'>Hasło</label>
-				<input type='password' name='password' id='passw' />
-
-				<label htmlFor='pass_confirm'>Powtórz hasło</label>
-				<input type='password' name='pass_confirm' id='pass_confirm' />
-
-				<button type='submit'>Stwórz konto</button> */}
+				<button disabled={isSubmitting} type='submit'>
+					{isSubmitting ? "Ładowanie..." : "Załóż konto"}
+				</button>
 			</form>
 			<Link to='/logowanie'>Masz już konto? Zaloguj się tutaj</Link>
 		</main>
